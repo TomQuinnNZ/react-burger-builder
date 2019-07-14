531d77bcdad2fe4f27486bbd2781ffd11bae1076
Tom Quinn <tomquinn@hotmail.co.nz>
2019-07-14 18:00:04 +1200

    create a SideDrawer component, and add it to layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import classes from './Layout.module.css';
 
 const layout = (props) => (
     <>
         <Toolbar></Toolbar>
-
+        <SideDrawer></SideDrawer>
+        
         <main className={classes.Content}>
             {props.children}
         </main>
---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer.module.css';
+
+const sideDrawer = (props) => {
+
+    // animations for in/out
+
+    return (
+        <div className={classes.SideDrawer}>
+            <Logo></Logo>
+            <nav>
+                <NavigationItems></NavigationItems>
+            </nav>
+        </div>
+    )
+};
+
+export default sideDrawer;
